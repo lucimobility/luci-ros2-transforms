5085f884c27193925835fe72cadad563a746f5a1
Christian Prather <christian@luci.com>
2022-06-29 17:01:08 -0600

    Got building with ROS2

--- permobil_m3_tf/src/tf_broadcaster.cpp
+++ permobil_m3_tf/src/tf_broadcaster.cpp
@@ -1,18 +1,41 @@
-#include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
+#include <rclcpp/rclcpp.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "robot_tf_publisher");
-    ros::NodeHandle node_handle;
-    ros::Rate rate(20);
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("robot_tf_publisher");
 
-    tf::TransformBroadcaster broadcaster;
+    //   ros::init(argc, argv, "robot_tf_publisher");
+    //   ros::NodeHandle node_handle;
+    rclcpp::Rate loop_rate(20);
 
-    while (ros::ok())
+    auto broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(node);
+    while (rclcpp::ok())
     {
-        broadcaster.sendTransform(
-            tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, -0.707, 0.707), tf::Vector3(0, 0, 0)), ros::Time::now(), "base_link", "base_camera"));
-        rate.sleep();
+        rclcpp::Time now = node->get_clock()->now();
+
+        geometry_msgs::msg::TransformStamped trans;
+        trans.header.stamp = now;
+        trans.header.frame_id = "base_link";
+        trans.child_frame_id = "base_camera";
+
+        trans.transform.translation.x = 0;
+        trans.transform.translation.y = 0;
+        trans.transform.translation.z = 0;
+        tf2::Quaternion quat;
+        // TODO feed in actual degrees based on camera angles 0, 0, -0.707, 0.707
+        quat.setRPY(0, 0, -0.707);
+        trans.transform.rotation.x = quat.x();
+        trans.transform.rotation.y = quat.y();
+        trans.transform.rotation.z = quat.z();
+        trans.transform.rotation.w = quat.w();
+
+        broadcaster->sendTransform(trans);
+        rclcpp::spin_some(node);
+        loop_rate.sleep();
     }
 }
