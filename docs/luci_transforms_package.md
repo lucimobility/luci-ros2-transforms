423f758df3ddb108b37087754f668ea78b844f85
Christian Prather <clemsbach@gmail.com>
2024-02-21 10:33:44 -0700

    Rewrote publisher to be static class

--- docs/luci_transforms_package.md
+++ docs/luci_transforms_package.md
@@ -4,6 +4,12 @@
 
 This package handles the transformation of raw sensor points that are all referenced at sensor origin to a common chair center origin.
 
+This package should only be needed if using the gRPC node to get LUCI transformed pointclouds into a ROS2 base_link compatible frame. This node essentially switches which axis is in the front of the chair since LUCI has Y axis in front and
+standard base_link has X axis in front. 
+
+If using a simulation with a URDF that includes accurate LUCI sensor placement and robot_description node this node should not be ran. In addition if the gRPC node is ever altered to produce the raw sensor streams (that is to say data that has not already
+been transformed by LUCI to be relative to its center of chair frame) then these transforms will no longer be valid.
+
 _**Note: All the sensors from LUCI streams are conveniently  already set relative to the chair center so the transforms below are an identity matrix that is only here to provide other tools such as nav\_2 and rviz2 with knowledge of the frames used**_
 
 | Package | Node |
--- quickie_500m_tf/src/tf_broadcaster.cpp
+++ quickie_500m_tf/src/tf_broadcaster.cpp
@@ -1,25 +1,60 @@
+/***
+ * Note: This file is NOT needed if using an accurate URDF as the transforms are handled by the
+ * robot_description node The transforms here are also mildly misleading as they suggest that the
+ * sensors are inline with the chairs base link and just need to be rotated 90 degrees on the chairs
+ * z axis. This is because the transforms in this file are for the gRPC streams which on LUCI have
+ * already been transformed to be in the chairs base_link frame. The 90 rotation on z in this file
+ * is because LUCI considers forward as the Y axis and ROS has forward as being the X axis. So
+ * points from gRPC that are in front of the chair will have a Y distance value and to get them
+ * compatible with ROS we rotate them to have a X axis value.
+ *
+ */
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <tf2/LinearMath/Quaternion.h>
-#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
 
-#include <geometry_msgs/msg/transform_stamped.hpp>
-
-int main(int argc, char** argv)
+/**
+ * @brief The static frame publisher used in conjunction with the gRPC interface node to transform
+ * the poinclouds to a ROS native axis for base_link.
+ *
+ */
+class StaticFramePublisher : public rclcpp::Node
 {
-    rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("robot_tf_publisher");
-
-    rclcpp::Rate loop_rate(20);
-
-    auto camera_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(node);
-    auto radar_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(node);
-    auto ultrasonic_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(node);
-
-    while (rclcpp::ok())
+  public:
+    /**
+     * @brief Construct a new Static Frame Publisher object
+     * Node name = "luci_tf_publisher"
+     *
+     */
+    explicit StaticFramePublisher() : Node("luci_tf_publisher")
     {
-        rclcpp::Time now = node->get_clock()->now();
+        /// Static transform broadcasters
+        camera_broadcaster = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+        radar_broadcaster = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+        ultrasonic_broadcaster = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+        footprint_broadcaster = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
-        // Camera base frame
+        /// Actually makes transformations and sends them
+        this->makeTransforms();
+    }
+
+  private:
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> camera_broadcaster;
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> radar_broadcaster;
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> ultrasonic_broadcaster;
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> footprint_broadcaster;
+
+    /**
+     * @brief Makes all transforms available to the ROS2 network
+     *
+     */
+    void makeTransforms()
+    {
+        // Get current time transforms are being published (time is ROS synced)
+        rclcpp::Time now = this->get_clock()->now();
+
+        // Camera stream frame
         geometry_msgs::msg::TransformStamped camera_tf;
         camera_tf.header.stamp = now;
         camera_tf.header.frame_id = "base_link";
@@ -28,13 +63,14 @@ int main(int argc, char** argv)
         camera_tf.transform.translation.y = 0;
         camera_tf.transform.translation.z = 0;
         tf2::Quaternion quat_camera;
-        // TODO feed in actual degrees based on camera angles 0, 0, -0.707, 0.707
-        quat_camera.setRPY(0, 0, 0);
+        quat_camera.setRPY(
+            0, 0, -1.5708); // Rotate from base_link to base_camera is -90 degrees in z axis
         camera_tf.transform.rotation.x = quat_camera.x();
         camera_tf.transform.rotation.y = quat_camera.y();
         camera_tf.transform.rotation.z = quat_camera.z();
         camera_tf.transform.rotation.w = quat_camera.w();
 
+        // Radar stream frame
         geometry_msgs::msg::TransformStamped radar_tf;
         radar_tf.header.stamp = now;
         radar_tf.header.frame_id = "base_link";
@@ -43,13 +79,14 @@ int main(int argc, char** argv)
         radar_tf.transform.translation.y = 0;
         radar_tf.transform.translation.z = 0;
         tf2::Quaternion quat_radar;
-        quat_radar.setRPY(0, 0, 0);
+        // Rotate from base_link to base_radar is -90 degrees in z axis
+        quat_radar.setRPY(0, 0, -1.5708);
         radar_tf.transform.rotation.x = quat_radar.x();
         radar_tf.transform.rotation.y = quat_radar.y();
         radar_tf.transform.rotation.z = quat_radar.z();
         radar_tf.transform.rotation.w = quat_radar.w();
 
-        // Ultrasonic base frame
+        // Ultrasonic stream frame
         geometry_msgs::msg::TransformStamped ultrasonic_tf;
         ultrasonic_tf.header.stamp = now;
         ultrasonic_tf.header.frame_id = "base_link";
@@ -58,16 +95,41 @@ int main(int argc, char** argv)
         ultrasonic_tf.transform.translation.y = 0;
         ultrasonic_tf.transform.translation.z = 0;
         tf2::Quaternion quat_ultrasonic;
-        quat_ultrasonic.setRPY(0, 0, 0);
+        // Rotate from base_link to base_ultrasonic is -90 degrees in z axis
+        quat_ultrasonic.setRPY(0, 0, -1.5708);
         ultrasonic_tf.transform.rotation.x = quat_ultrasonic.x();
         ultrasonic_tf.transform.rotation.y = quat_ultrasonic.y();
         ultrasonic_tf.transform.rotation.z = quat_ultrasonic.z();
         ultrasonic_tf.transform.rotation.w = quat_ultrasonic.w();
 
-        camera_broadcaster->sendTransform(camera_tf);
-        radar_broadcaster->sendTransform(radar_tf);
-        ultrasonic_broadcaster->sendTransform(ultrasonic_tf);
-        rclcpp::spin_some(node);
-        loop_rate.sleep();
+        // Base footprint
+        geometry_msgs::msg::TransformStamped footprint_tf;
+        footprint_tf.header.stamp = now;
+        footprint_tf.header.frame_id = "base_link";
+        footprint_tf.child_frame_id = "base_footprint";
+        footprint_tf.transform.translation.x = 0;
+        footprint_tf.transform.translation.y = 0;
+        // Translation from base_link to base_footprint is -0.17 meters on z axis (how high is
+        // center of chair from the ground)
+        footprint_tf.transform.translation.z = -0.17;
+        tf2::Quaternion quat_footprint;
+        quat_footprint.setRPY(0, 0, 0);
+        footprint_tf.transform.rotation.x = quat_footprint.x();
+        footprint_tf.transform.rotation.y = quat_footprint.y();
+        footprint_tf.transform.rotation.z = quat_footprint.z();
+        footprint_tf.transform.rotation.w = quat_footprint.w();
+
+        this->camera_broadcaster->sendTransform(camera_tf);
+        this->radar_broadcaster->sendTransform(radar_tf);
+        this->ultrasonic_broadcaster->sendTransform(ultrasonic_tf);
+        this->footprint_broadcaster->sendTransform(footprint_tf);
     }
+};
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<StaticFramePublisher>());
+    rclcpp::shutdown();
+    return 0;
 }
